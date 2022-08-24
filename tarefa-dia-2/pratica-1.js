fe4ed3d22059cc96beb63215ac2ce0ddde0f8c13
Rodrigo Figueiredo <rodrigo84@outlook.com>
2022-08-24 11:34:35 -0300

    Update pratica-1.js
    atualização da mensagem para a agência de turismo

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -5,3 +5,5 @@ let age = 38;
 let bestPlace = "Holanda"
 
 console.log(`Seu e-mail: ${email}, Seu nome: ${nome}, Ocupação: ${ocupation}, sua idade: ${age}, Sonho de conhecer: ${bestPlace}`)
+
+console.log(`Olá, ${nome}. Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${bestPlace}. Confirma pra gente alguns dados? A sua idade é ${age}, você é ${ocupation} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
